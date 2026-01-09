9dd94a913453cc35fc7c146f6045433c559b3e75
Susy <ksusana.valderrama@gmail.com>
2026-01-09 07:11:18 -0600

    Generacion histograma

---
+++ app.py
@@ -0,0 +1,23 @@
+'''Librerias que se utilizaran'''
+import streamlit as st
+import pandas as pd
+import plotly_express as px
+st.header('Proyecto Sprint 7')
+car_data = pd.read_csv('vehicles_us.csv') # leer los datos
+
+# crear una casilla de verificación
+build_histogram = st.checkbox('Construir un histograma')
+
+if build_histogram: # si la casilla de verificación está seleccionada
+    st.write('Construir un histograma para la columna odómetro')
+hist_button = st.button('Construir histograma') # crear un botón
+
+if hist_button: # al hacer clic en el botón
+    # escribir un mensaje
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+
+    # crear un histograma
+    fig = px.histogram(car_data, x="odometer")
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
